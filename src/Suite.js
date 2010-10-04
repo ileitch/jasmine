05d20c135a72a49190a846bd8539fa6bce6c3c1f
Ian Leitch <port001@gmail.com>
2010-10-04 14:02:45 +1100

    Use finish method so that that reporter is notified

--- src/Suite.js
+++ src/Suite.js
@@ -80,10 +80,10 @@ jasmine.Suite.prototype.children = function() {
 };
 
 jasmine.Suite.prototype.execute = function(onComplete) {
-  for (i = 0; i < this.tags_.length; i++) {
-    for (n = 0; n < this.env.disabledTags().length; n++) {
+  for (var i = 0; i < this.tags_.length; i++) {
+    for (var n = 0; n < this.env.disabledTags().length; n++) {
       if (this.tags_[i] == this.env.disabledTags()[n]) {
-        this.finished = true;
+        this.finish();
         return;
       }
     }
